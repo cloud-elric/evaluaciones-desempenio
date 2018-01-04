0a3645cc2d4181860a1cf18731f0c4b4f3955182
Humberto Antonio Marquez <humberto@2gom.com.mx>
2018-01-04 16:38:05 -0600

    Correccion de email

--- mail/render/sendEmailEvaluacion.php
+++ mail/render/sendEmailEvaluacion.php
@@ -1,149 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
 <html>
-<head>
-<title>Mailing-Evaluaciones-Rosa</title>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
-<!-- Save for Web Slices (Mailing-Evaluaciones-Rosa.psd) -->
-<table id="Table_01" width="1189" height="737" border="0" cellpadding="0" cellspacing="0">
-	<tr>
-		<td rowspan="14">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_01.gif" width="154" height="736" alt=""></td>
-		<td rowspan="14">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/side-image.jpg" width="222" height="736" alt=""></td>
-		<td colspan="2" rowspan="5">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/side-image-hair.gif" width="137" height="285" alt=""></td>
-		<td colspan="3" rowspan="3">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_04.gif" width="156" height="136" alt=""></td>
-		<td colspan="2" rowspan="5">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/panel-right-side.gif" width="139" height="285" alt=""></td>
-		<td colspan="3">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_06.gif" width="380" height="70" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="70" alt=""></td>
-	</tr>
-	<tr>
-		<td rowspan="13">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_07.gif" width="27" height="666" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/corner-top-right.gif" width="11" height="15" alt=""></td>
-		<td rowspan="13">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_09.gif" width="342" height="666" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="15" alt=""></td>
-	</tr>
-	<tr>
-		<td rowspan="8">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_10.gif" width="11" height="514" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="51" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="3">
-			<a href="http://www.licuadoragroup.com/" target="_blank">
-				<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Logo-Cliente.jpg" width="156" height="70" border="0" alt=""></a></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="70" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="3">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_12.gif" width="156" height="79" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="79" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="7">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/text-1.gif" width="432" height="55" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="55" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="7">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_14.gif" width="432" height="96" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="96" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="7">
-			<a href="http://www.2geeksonemonkey.com" target="_blank">
-				<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/boton-cta.gif" width="432" height="46" border="0" alt=""></a></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="46" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="7">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/text-disclaimer.gif" width="432" height="115" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="115" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="7" rowspan="3">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_17.gif" width="432" height="56" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="2" alt=""></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/corner-bottom-right.gif" width="11" height="14" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="14" alt=""></td>
-	</tr>
-	<tr>
-		<td rowspan="3">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_19.gif" width="11" height="123" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="40" alt=""></td>
-	</tr>
-	<tr>
-		<td rowspan="2">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_20.gif" width="94" height="83" alt=""></td>
-		<td colspan="5">
-			<a href="http://www.2geeksonemonkey.com" target="_blank">
-				<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/desarrollado-por-.gif" width="251" height="20" border="0" alt=""></a></td>
-		<td rowspan="2">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_22.gif" width="87" height="83" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="20" alt=""></td>
-	</tr>
-	<tr>
-		<td colspan="2">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_23.gif" width="112" height="63" alt=""></td>
-		<td>
-			<a href="http://www.2geeksonemonkey.com" target="_blank">
-				<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/2-Geeks-one-Monkey.jpg" width="28" height="63" border="0" alt=""></a></td>
-		<td colspan="2">
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/Mailing-Evaluaciones-Rosa-3_25.gif" width="111" height="63" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="1" height="63" alt=""></td>
-	</tr>
-	<tr>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="154" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="222" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="94" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="43" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="69" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="28" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="59" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="52" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="87" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="27" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="11" height="1" alt=""></td>
-		<td>
-			<img src="https://qa.2geeksonemonkey.com/mailings/licuadora/images/spacer.gif" width="342" height="1" alt=""></td>
-		<td></td>
-	</tr>
-</table>
-<!-- End Save for Web Slices -->
-</body>
+  <head>
+    <title>Mailing-Evaluaciones-Rosa</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+  </head>
+  <body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+    <!-- Save for Web Slices (Mailing-Evaluaciones-Rosa.psd) -->
+    <table id="Table_01" width="1188" height="736" border="0" cellpadding="0" cellspacing="0">
+      <tr>
+        <td rowspan="8" style="margin:0;padding:0;width:377px; height:736px;">
+          <img src="https://dev.2geeksonemonkey.com/testfiles/images/mailing-img_01.jpg" width="377" height="736" alt="">
+        </td>
+        <td style="margin:0;padding:0;width:430px; height:285px;">
+          <img src="https://dev.2geeksonemonkey.com/testfiles/images/mailing-img_02.jpg" width="430" height="285" alt="">
+        </td>
+        <td rowspan="8" style="margin:0;padding:0;width:381px; height:736px;">
+          <img src="https://dev.2geeksonemonkey.com/testfiles/images/mailing-img_03.gif" width="381" height="736" alt="">
+        </td>
+      </tr>
+      <tr>
+        <td style="margin:0;padding:0;width:430px;height:41px;">
+          <h3 style="margin:0;padding:0;background-color:#fff;font-family: 'verdana','sans-serif';font-size:16px;text-align: justify; color:#000;">Hola <?=$nombre?></h3>
+        </td>
+      </tr>
+      <tr>
+        <td style="margin:0;padding:0;width:430px;height:110px;">
+          <p style="margin:0;padding:0;width:430px;height:90px;background-color:#fff;font-family: 'verdana','sans-serif';font-size: 12px; text-align: left; color:#434343;overflow: hidden; vertical-align: top;">Te invitamos a realizar las evaluaciones de desempeño de tus compañeros, colaboradores (si aplica), y la tuya dando click en el siguiente bóton</p>
+        </td>
+      </tr>
+      <tr>
+        <td style="margin:0;padding:0;width:430px;height:45px;background-color: #32b2a5;text-align:center">
+          <a href="<?=$url?>" target="_blank" style="line-height: 45px;margin:0;padding:0;display:block;width:100%;height:100%;background-color: #32b2a5;color:#FFF;text-decoration: none;letter-spacing: 1px; font-family: 'verdana','sans-serif';font-size: 14px; text-align: center;font-weight: 800;">
+            Ir a mi evaluación</a>
+        </td>
+      </tr>
+      <tr>
+        <td style="margin:0;padding:0;width:430px;height:130px;">
+          <p style="margin:0;padding:0;background-color:#fff;font-family: 'verdana','sans-serif';font-size: 9px; text-align: justify; color:#000;">Estas recibiendo este correo porque perteneces a una lista de correos los cuales fueron seleccionados por “Licuadora” para el envío de las ligas correspondientes al servicio de evaluaciones del desempeño por parte de un consultor independiente especializado para garantizar la confidencialidad y objetividad de la encuesta y sus resultados.</p>
+          <p style="margin:0;padding:0;background-color:#fff;font-family: 'verdana','sans-serif';font-size: 9px; text-align: justify; color:#000;">Este correo es parte de un servicio de correo auto generado por nuestro sistema por lo que no es necesario que conteste a este correo, en caso de dudas consulte la ventana de contacto dentro del portal.</p>
+        </td>
+      </tr>
+      <tr>
+        <td style="margin:0;padding:0;width:430px;height:32px;background-color: #fc424f;"></td>
+      </tr>
+      <tr>
+        <td style="text-align: center;
+		background: #fc424f;margin:0;padding:0;width:430px; height:32px;">
+          <a href="http://www.2geeksonemonkey.com" target="_blank" style="color:white;margin:0;padding:0;border:none; padding:0;background-color:#fc424f;text-decoration: none;font-family: 'verdana','sans-serif';font-size: 9px; text-align: center;">
+            Sistema desarrollado por 2 Geeks one Monkey ©2018
+          </a>
+        </td>
+      </tr>
+      <tr>
+        <td style="margin:0;padding:0;width:430px; height:60px;">
+          <a href="http://www.2geeksonemonkey.com" target="_blank" style="margin:0;padding:0;">
+            <img src="https://dev.2geeksonemonkey.com/testfiles/images/2geeksonemonkey.jpg" width="430" height="60" alt=""></a>
+        </td>
+      </tr>
+    </table>
+    <!-- End Save for Web Slices -->
+  </body>
 </html>
